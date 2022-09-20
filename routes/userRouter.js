50feda30321c67212355771ac565010673750822
Mark Sarkis <mark.sarkis93@gmail.com>
2022-09-20 13:19:49 -0700

    update user router, add axios to react

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,11 @@
 const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
 const Router = require('koa-router');
 const router = new Router();
 const knex = require('../knex/knex.js');
 
-// TODO: add auth
+// TODO: add middleware to be sure only certain roles + user can access this info
+// get user ID
 router.get('/api/user/:id', async ctx => {
     const user = await knex('users')
         .select({
@@ -26,12 +28,11 @@ router.get('/api/user/:id', async ctx => {
     if(!user) {
         return ctx.throw(400, 'User not found!');
     }
-    return user;
+    return ctx.body = user;
 });
 
 // endpoint for signing in to the app
-router.post('/api/signin', async ctx => {
-    // convert password to hash and compare to password in DB
+router.post('/api/user/signin', async ctx => {
     // check if user exists
     const result = await knex('users')
     .select('*')
@@ -53,17 +54,43 @@ router.post('/api/signin', async ctx => {
         return ctx.throw(400, 'Email not found');
     };
     
-
+    // convert password to hash and compare to password in DB
     const pwdsMatch = await argon2.verify(result.password, ctx.request.body.password);
 
-    if(pwdsMatch){
-        return ctx.body = "success";
-        // TODO: add cookie to keep track of logged in user
-        // now run the function to place cookie for verification!
+    // if pwds match and we know the user ID
+    if(pwdsMatch && result.id){
+        // create JWT token when user logs in
+        const token = jwt.sign({user: result.id}, process.env.JWT_SECRET);
+        // set cookie to keep track of logged in user
+        const cookieInfo = { 
+            httpOnly: true,
+            sameSite: "lax"
+        };
+        if(process.env.NODE_ENV === 'production') {
+            cookieInfo.secure = true;
+        }
+        
+        // don't send password to client!
+        // TODO: include user role in result when table gets added so we can redirect them to proper page
+        delete result.password;
+
+        // set cookie so we have a way to authenticate next calls!
+        ctx.cookies.set('app_template_cookie', token, cookieInfo);
+        
+        // TODO: replace this with 0Auth or something more secure when we're going to do an enterprise app
+        // https://www.passportjs.org/packages/passport-local/
+        return ctx.body = {user: result};
+
+        // below is how to GET the cookie we just set back...
+        // console.log(ctx.cookies.get('app_template_cookie'), ' get cookies')
     } else {
         // tell user their password is wrong
         return ctx.throw(400, 'Incorrect Password');
     }
+});
+
+router.get("/api/super-secret-resource", async ctx => {
+    return ctx.body = { message: "You need to be logged in to access this secret place" };
 });
 
 module.exports = router;
